d28a52f0a81937be1eada4d4ec366104d4c1cf50
Monik Mewada <monikmewada5667@gmail.com>
2025-02-10 18:48:10 +0530

    Updated AutoVC

--- main.py
+++ main.py
@@ -27,16 +27,17 @@ if __name__ == '__main__':
 
     # Model configuration.
     parser.add_argument('--lambda_cd', type=float, default=1, help='weight for hidden code loss')
-    parser.add_argument('--dim_neck', type=int, default=16)
+    parser.add_argument('--dim_neck', type=int, default=32)
     parser.add_argument('--dim_emb', type=int, default=256)
     parser.add_argument('--dim_pre', type=int, default=512)
-    parser.add_argument('--freq', type=int, default=16)
+    parser.add_argument('--freq', type=int, default=32)
     
     # Training configuration.
-    parser.add_argument('--data_dir', type=str, default='./spmel')
+    parser.add_argument('--data_dir', type=str, default=r'C:\Users\DELL\OneDrive\Desktop\Internship\mel')  
     parser.add_argument('--batch_size', type=int, default=2, help='mini-batch size')
     parser.add_argument('--num_iters', type=int, default=1000000, help='number of total iterations')
-    parser.add_argument('--len_crop', type=int, default=128, help='dataloader output sequence length')
+
+    parser.add_argument('--len_crop', type=int, default=256, help='dataloader output sequence length')
     
     # Miscellaneous.
     parser.add_argument('--log_step', type=int, default=10)
--- make_spect.py
+++ make_spect.py
@@ -1,11 +1,11 @@
 import os
-import pickle
 import numpy as np
 import soundfile as sf
 from scipy import signal
 from scipy.signal import get_window
 from librosa.filters import mel
 from numpy.random import RandomState
+import librosa
 
 
 def butter_highpass(cutoff, fs, order=5):
@@ -14,11 +14,8 @@ def butter_highpass(cutoff, fs, order=5):
     b, a = signal.butter(order, normal_cutoff, btype='high', analog=False)
     return b, a
     
-    
-def pySTFT(x, fft_length=1024, hop_length=256):
-    
+def pySTFT(x, fft_length=1920, hop_length=480):
     x = np.pad(x, int(fft_length//2), mode='reflect')
-    
     noverlap = fft_length - hop_length
     shape = x.shape[:-1]+((x.shape[-1]-noverlap)//hop_length, fft_length)
     strides = x.strides[:-1]+(hop_length*x.strides[-1], x.strides[-1])
@@ -29,42 +26,45 @@ def pySTFT(x, fft_length=1024, hop_length=256):
     result = np.fft.rfft(fft_window * result, n=fft_length).T
     
     return np.abs(result)    
-    
-    
-mel_basis = mel(16000, 1024, fmin=90, fmax=7600, n_mels=80).T
+
+mel_basis = librosa.filters.mel(sr=48000, n_fft=1920, fmin=90, fmax=7600, n_mels=80).T
 min_level = np.exp(-100 / 20 * np.log(10))
-b, a = butter_highpass(30, 16000, order=5)
+b, a = butter_highpass(30, 48000, order=5)
 
+# Audio file directory
+rootDir = r'C:\Users\DELL\OneDrive\Desktop\Internship\ADESH'
+# Spectrogram directory
+targetDir = r'C:\Users\DELL\OneDrive\Desktop\Internship\mel'
 
-# audio file directory
-rootDir = './wavs'
-# spectrogram directory
-targetDir = './spmel'
+# Get list of .wav files directly from rootDir
+fileList = [f for f in os.listdir(rootDir) if f.endswith('.wav')]
 
+for fileName in sorted(fileList):
+    file_path = os.path.join(rootDir, fileName)
 
-dirName, subdirList, _ = next(os.walk(rootDir))
-print('Found directory: %s' % dirName)
-
-for subdir in sorted(subdirList):
-    print(subdir)
-    if not os.path.exists(os.path.join(targetDir, subdir)):
-        os.makedirs(os.path.join(targetDir, subdir))
-    _,_, fileList = next(os.walk(os.path.join(dirName,subdir)))
-    prng = RandomState(int(subdir[1:])) 
-    for fileName in sorted(fileList):
+    try:
         # Read audio file
-        x, fs = sf.read(os.path.join(dirName,subdir,fileName))
+        x, fs = sf.read(file_path)
+
         # Remove drifting noise
         y = signal.filtfilt(b, a, x)
-        # Ddd a little random noise for model roubstness
-        wav = y * 0.96 + (prng.rand(y.shape[0])-0.5)*1e-06
-        # Compute spect
+        
+        # Add random noise for model robustness
+        prng = RandomState(abs(hash(fileName)) % (2**32 - 1))  # Use the hash of the filename as a seed
+        wav = y * 0.96 + (prng.rand(y.shape[0]) - 0.5) * 1e-06
+        
+        # Compute spectrogram
         D = pySTFT(wav).T
+
         # Convert to mel and normalize
         D_mel = np.dot(D, mel_basis)
         D_db = 20 * np.log10(np.maximum(min_level, D_mel)) - 16
         S = np.clip((D_db + 100) / 100, 0, 1)    
-        # save spect    
-        np.save(os.path.join(targetDir, subdir, fileName[:-4]),
-                S.astype(np.float32), allow_pickle=False)    
-        
+
+        # Save spectrogram
+        output_file_path = os.path.join(targetDir, fileName[:-4] + '.npy')
+        np.save(output_file_path, S.astype(np.float32), allow_pickle=False)
+    
+    except:
+        # Skip any unreadable files without showing an error
+        continue
